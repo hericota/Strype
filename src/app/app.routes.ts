3c8c07503bb0802ea5b9c55bc51fcb22ec3605b1
jordan <jordansnatos@gmail.com>
2026-09-03 16:57:40 -0300

    ajusta html e ajusta ts e termina estilização

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { PostProdutos } from './feats/posts/post-produtos/post-produtos';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'CadastroProduto', component:PostProdutos},
+];
--- src/app/feats/posts/post-produtos/post-produtos.ts
+++ src/app/feats/posts/post-produtos/post-produtos.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { form, required, FormField } from '@angular/forms/signals';
+import { Produto } from '../produto';
 
 @Component({
   selector: 'app-post-produtos',
-  imports: [],
+  imports: [FormField],
   templateUrl: './post-produtos.html',
   styleUrl: './post-produtos.css',
 })
 export class PostProdutos {
 
+  cadastroModel = signal<Produto>({
+    nome: '',
+    descricao: '',
+    preco: null,
+    urlImagem: ''
+  })
+
+  cadastroForm = form(this.cadastroModel, (s)=>{
+    required(s.nome, ({message:'Campo obrigatório'}))
+    required(s.descricao, ({message:'Campo obrigatório'}))
+    required(s.preco, ({message:'Campo obrigatório'}))
+    required(s.urlImagem, ({message:'Campo obrigatório'}))
+  })
+
+  cadastrarProduto(event: SubmitEvent){
+    event.preventDefault();
+
+    const produto = this.cadastroModel();
+
+    
+  }
 }
