aa0044aedb002f92707e45bb2b7771fee917438e
Arthur Oliver <arthurolivermachado@gmail.com>
2026-09-03 16:43:59 -0300

    Cria rotas, ajusta html da nav, adiciona texto a search-div e formata hero-container

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: 'Home', pathMatch: 'full'},
+    {path: 'Home', component: Home },
+    { path: '**', redirectTo: 'home' },
+];
