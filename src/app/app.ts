8a75f00bd87b8e93c4823a03dd223966bb7abe6f
Ruan Carlos Leite <140746395+ruancarlosleite@users.noreply.github.com>
2026-09-04 17:21:16 -0300

    Finaliza feature favoritos

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Favoritos } from './favoritos/favoritos';
 
 @Component({
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Favoritos],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
 })
 export class App {
-  protected readonly title = signal('stryde');
+  protected readonly title = signal('pagina-favoritos');
 }
