1a61254c6198a6606254fb85ad4adb5b4a5b2a8f
ANA BEATRIZ <oibibih@gmail.com>
2026-09-08 14:31:16 -0300

    feats/Cria formulario da tela de cadastro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,14 @@ import { Routes } from '@angular/router';
 import { PostProdutos } from './feats/posts/post-produtos/post-produtos';
 import { TelaAdmin } from './feats/tela-admin/tela-admin';
 import { Home } from './home/home';
+import { CadastroComponente } from './feats/cadastro-componente/cadastro-componente';
+import { LoginComponente } from './feats/login-componente/login-componente';
 
 export const routes: Routes = [
     {path:"telaAdmin", component:TelaAdmin},
     {path: '', redirectTo: 'Home', pathMatch: 'full'},
     {path: 'Home', component: Home },
     { path: '**', redirectTo: 'home' },
+    {path: 'cadastro', component: CadastroComponente},
+    {path: 'login', component: LoginComponente},
 ];
---
+++ src/app/feats/cadastro-componente/cadastro-form/cadastro-form.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-cadastro-form',
+  styleUrl: './cadastro-form.css',
+  templateUrl: './cadastro-form.html',
+})
+export class CadastroForm {}
