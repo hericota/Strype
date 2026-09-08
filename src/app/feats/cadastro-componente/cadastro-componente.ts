67c93369309134d19fa2e8acaf929060f054ddf7
ANA BEATRIZ <oibibih@gmail.com>
2026-09-08 17:08:31 -0300

    style: cadastro form css

--- src/app/feats/cadastro-componente/cadastro-componente.ts
+++ src/app/feats/cadastro-componente/cadastro-componente.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { CadastroForm } from "./cadastro-form/cadastro-form";
+import { RouterLink } from '@angular/router';
 
 @Component({
-  imports: [CadastroForm],
+  imports: [CadastroForm, RouterLink],
   selector: 'app-cadastro-componente',
   styleUrl: './cadastro-componente.css',
   templateUrl: './cadastro-componente.html',
