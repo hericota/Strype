51d85ce4f1b440a9047077d223f64129b6ed6b70
Henrique Galvão Dall Alba <henriquegdall@gmail.com>
2026-09-08 13:41:19 -0300

    arrumando rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,17 @@ import { Routes } from '@angular/router';
 import { PostProdutos } from './feats/posts/post-produtos/post-produtos';
 import { TelaAdmin } from './feats/tela-admin/tela-admin';
 import { Home } from './home/home';
+import { Favoritos } from './favoritos/favoritos';
 
 export const routes: Routes = [
     {path:"telaAdmin", component:TelaAdmin},
+
+    {path:"produtos", component:PostProdutos},
+
+    {path:"favoritos", component:Favoritos},
+
+
+
     {path: '', redirectTo: 'Home', pathMatch: 'full'},
     {path: 'Home', component: Home },
     { path: '**', redirectTo: 'home' },
