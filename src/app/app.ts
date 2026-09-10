98c7e8b879ad0610706f62c6e857a23dd2b7eb8e
Ruan Carlos Leite <140746395+ruancarlosleite@users.noreply.github.com>
2026-09-10 13:57:18 -0300

    atualizando

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,9 +2,10 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Home } from "./home/home";
 import { PostProdutos } from './feats/posts/post-produtos/post-produtos';
+import { Header } from "./components/header/header";
 
 @Component({
-  imports: [RouterOutlet, Home, PostProdutos],
+  imports: [RouterOutlet, Home, PostProdutos, Header],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
