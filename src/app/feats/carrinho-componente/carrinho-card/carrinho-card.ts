9aee8cf1874023fbc8d6f38a268df6c571d83f4a
ANA BEATRIZ <oibibih@gmail.com>
2026-09-09 22:54:37 -0300

    feat(carrinho): implementa layout estrutural do card de itens com imagem, botões de ação e resumo de valores

--- src/app/feats/carrinho-componente/carrinho-card/carrinho-card.ts
+++ src/app/feats/carrinho-componente/carrinho-card/carrinho-card.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
-  imports: [],
+  imports: [RouterLink],
   selector: 'app-carrinho-card',
   styleUrl: './carrinho-card.css',
   templateUrl: './carrinho-card.html',
 })
-export class CarrinhoCard {}
+export class CarrinhoCard {
+
+}
