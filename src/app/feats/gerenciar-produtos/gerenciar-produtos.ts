1721a0b9bdebb6bd18f5da2af0cf1837ea5670eb
Henrique Galvão Dall Alba <henriquegdall@gmail.com>
2026-09-11 14:05:10 -0300

    criando componente gereciar-produtos

---
+++ src/app/feats/gerenciar-produtos/gerenciar-produtos.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    imports: [],
+    selector: 'app-gerenciar-produtos',
+    styleUrl: './gerenciar-produtos.css',
+    templateUrl: './gerenciar-produtos.html',
+})
+export class GerenciarProdutos {}
