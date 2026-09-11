99061123421ed6ba3cda54c66b89d9ca91e959e8
Henrique Galvão Dall Alba <henriquegdall@gmail.com>
2026-09-11 15:47:09 -0300

    começando a integrar api
    adcionei os produtos porem ainda falta a pesquisa e atualizar

--- src/app/feats/atualizar-produto/atualizar-produto.ts
+++ src/app/feats/atualizar-produto/atualizar-produto.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Produto } from '../posts/produto';
+import { httpResource } from '@angular/common/http';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
     imports: [],
@@ -6,4 +9,15 @@ import { Component } from '@angular/core';
     styleUrl: './atualizar-produto.css',
     templateUrl: './atualizar-produto.html',
 })
-export class AtualizarProduto {}
+export class AtualizarProduto {
+    
+      private route = inject(ActivatedRoute);
+      id = this.route.snapshot.paramMap.get('id')
+      private readonly urlApi = 'http://localhost:8080/produtos';
+      readonly produtoEspecifico = httpResource<Produto>(
+        () => this.urlApi + "/" + this.id
+    )
+    atualizar(){
+
+    }
+}
