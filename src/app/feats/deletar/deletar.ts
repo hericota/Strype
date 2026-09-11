b44c02c82643f376e54f576692c251c4153526f4
Henrique Galvão Dall Alba <henriquegdall@gmail.com>
2026-09-11 16:59:16 -0300

    preparando delete
    para resolver amanha

--- src/app/feats/deletar/deletar.ts
+++ src/app/feats/deletar/deletar.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { DeleteInterface } from './delete-interface';
+import { form, required, FormField } from '@angular/forms/signals';
+import { Deletando } from './deletando';
 
 @Component({
-  imports: [],
+  imports: [FormField],
   selector: 'app-deletar',
   styleUrl: './deletar.css',
   templateUrl: './deletar.html',
 })
 export class Deletar {
-  
+  protected readonly consumoService = inject(Deletando)
+
+  deleteModel = signal<DeleteInterface>({
+    id:null,
+  });
+  deleteForm = form(this.deleteModel, (s) => {
+    required(s.id, { message: "Campo vazio" })
+  })
+
+  submitDelete(event: SubmitEvent) {
+    event.preventDefault()
+
+    const deletar = this.deleteModel();
+    this.consumoService.cadastrarDelete(deletar).subscribe({
+      next: () => {
+        this.deleteModel.set({
+          id:null
+        });
+        console.log("Você conseguiu")
+        this.deleteForm().reset()
+      },
+      error:()=>{
+        console.log("ta tudo errado")
+      }
+    })
+  }
 }
