54d36db3d2c517816c8be8bf4891772c512513db
Arthur Oliver <arthurolivermachado@gmail.com>
2026-09-11 14:18:16 -0300

    Adiciona estrutura html de banner promocional

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 import { Header } from "../components/header/header";
 import { Footer } from "../components/footer/footer";
 import { Hero } from '../components/hero/hero';
+import { RouterLink } from '@angular/router';
 
 @Component({
-  imports: [Header, Footer, Hero],
+  imports: [Header, Footer, Hero, RouterLink],
   selector: 'app-home',
   styleUrl: './home.css',
   templateUrl: './home.html',
