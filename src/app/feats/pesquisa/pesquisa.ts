4499d14de67e3d8fd281bae564c1218047dcc761
Ruan Carlos Leite <140746395+ruancarlosleite@users.noreply.github.com>
2026-09-12 00:09:35 -0300

    feats/pesquisa componente

---
+++ src/app/feats/pesquisa/pesquisa.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    imports: [],
+    selector: 'app-pesquisa',
+    styleUrl: './pesquisa.css',
+    templateUrl: './pesquisa.html',
+})
+export class Pesquisa {}
