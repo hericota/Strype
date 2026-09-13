2f29e77ec83ff59e4274d8ca79a3888adecba2c4
ANA BEATRIZ <oibibih@gmail.com>
2026-09-13 00:35:52 -0300

    feat: adiciona tela de sucesso customizada

--- src/app/feats/carrinho-componente/carrinho-card/carrinho-card.ts
+++ src/app/feats/carrinho-componente/carrinho-card/carrinho-card.ts
@@ -6,32 +6,43 @@ import { CarrinhoService } from './carrinhoService/carrinho-service';
 import { CommonModule } from '@angular/common';
 
 @Component({
-    imports: [RouterLink,CommonModule], //commonModule faz com q eu consiga padronizar a formatação de valores no html usando o | number:'1.2-2'
+    imports: [RouterLink, CommonModule], //commonModule faz com q eu consiga padronizar a formatação de valores no html usando o | number:'1.2-2'
     selector: 'app-carrinho-card',
     styleUrl: './carrinho-card.css',
     templateUrl: './carrinho-card.html',
 })
 export class CarrinhoCard {
-    protected carrinhoService =  inject(CarrinhoService);
+    protected carrinhoService = inject(CarrinhoService);
 
     carrinho = this.carrinhoService.carrinhoModel;
 
-   //mudei a logica do carrinho para o service !!!
+    //mudei a logica do carrinho para o service !!!
 
-   aumentar(produtoId: number){ //pega a função aumentar que esta no service 
-    this.carrinhoService.aumentar(produtoId)
-   }
+    aumentar(produtoId: number) {
+        //pega a função aumentar que esta no service
+        this.carrinhoService.aumentar(produtoId);
+    }
 
-   diminuir(produtoId:number){ //pega a função diminuir que esta no service 
-    this.carrinhoService.diminuir(produtoId)
-   }
+    diminuir(produtoId: number) {
+        //pega a função diminuir que esta no service
+        this.carrinhoService.diminuir(produtoId);
+    }
 
-   excluir(produtoId:number){//pega a função excluir que esta no service 
-    this.carrinhoService.excluir(produtoId)
-   }
+    excluir(produtoId: number) {
+        //pega a função excluir que esta no service
+        this.carrinhoService.excluir(produtoId);
+    }
 
-  comprar() {
-    alert('Parabéns, sua compra foi finalizada!');
-    this.carrinhoService.carrinhoModel.set([]); // Limpa o carrinho
-}
+    
+    // caso a compra seja finalizada
+    compraFinalizada = false;
+
+    comprar() {
+        this.compraFinalizada = true;
+        this.carrinhoService.carrinhoModel.set([]); // Limpa o carrinho
+    }
+  
+    voltarCompras() {
+        this.compraFinalizada = false;
+    }
 }
