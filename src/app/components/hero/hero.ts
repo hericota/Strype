c6174e4c0b53d33b11167a7e1c6d0beadfa72d21
Henrique Galvão Dall Alba <henriquegdall@gmail.com>
2026-09-14 14:22:44 -0300

    hero

--- src/app/components/hero/hero.ts
+++ src/app/components/hero/hero.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
     imports: [RouterLink, FormsModule],
